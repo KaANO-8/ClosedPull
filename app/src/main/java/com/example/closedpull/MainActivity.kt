790b107a94d87b2699c288573d460d541cef2c3c
KaANO-8 <kaanorahul8@gmail.com>
2022-06-25 01:57:07 +0530

    project init

---
+++ app/src/main/java/com/example/closedpull/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.closedpull
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.closedpull.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
