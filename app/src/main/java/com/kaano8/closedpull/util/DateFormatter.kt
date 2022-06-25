44ba91a444a773d8c14f90bd6eb95b1968c44803
KaANO-8 <kaanorahul8@gmail.com>
2022-06-26 03:34:19 +0530

    fixed date formatting

--- app/src/main/java/com/kaano8/closedpull/util/DateFormatter.kt
+++ app/src/main/java/com/kaano8/closedpull/util/DateFormatter.kt
@@ -1,10 +1,24 @@
 package com.kaano8.closedpull.util
 
+import android.util.Log
+import java.text.ParseException
+import java.text.SimpleDateFormat
+
 object DateFormatter {
+    private const val TAG = "DateFormatter"
+
     fun formatDate(
         inputDate: String,
-        inputFormat: String = "YYYY-MM-DD"
+        inputFormat: String = "yyyy-MM-dd'T'HH:mm:ss",
+        outputFormat: String = "dd-MMM-yyyy"
     ): String {
-        return ""
+        val inputFormatter = SimpleDateFormat(inputFormat)
+        try {
+            val date = inputFormatter.parse(inputDate.dropLast(1))
+            return SimpleDateFormat(outputFormat).format(date)
+        } catch (exception: ParseException) {
+            Log.d(TAG, "${exception.message}")
+        }
+        return inputDate
     }
 }
